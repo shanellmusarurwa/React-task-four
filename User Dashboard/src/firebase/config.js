39c85de73a591b486efb4728b071ce93e9adb40c
Mitchelle <shanellmusarurwa6@gmail.com>
2025-07-10 00:46:08 +0200

    changed my .env file

--- User Dashboard/src/firebase/config.js
+++ User Dashboard/src/firebase/config.js
@@ -8,15 +8,14 @@ import {
   updateProfile,
 } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAwbff19TWIO036-5BSwKCBE-7sVpB-Ly8",
-  authDomain: "dashboard-task-c5e38.firebaseapp.com",
-  projectId: "dashboard-task-c5e38",
-  storageBucket: "dashboard-task-c5e38.firebasestorage.app",
-  messagingSenderId: "705588466174",
-  appId: "1:705588466174:web:a8ca1b9ca2212dbf9a70af",
 
- 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
